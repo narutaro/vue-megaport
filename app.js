d327aa7818f38f186b54961cd1b50e3d88ec9f2b
narutaro <narusan@mac.com>
2018-06-24 23:12:38 +0000

    bit secure

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-var megaport = new mp('https://api-staging.megaport.com/v2/');
-//var megaport = new Megaport('https://api.megaport.com/v2/');
+var megaport = new mp('https://api-staging.megaport.com/v2/'); // with OLD version megaport.js
+//var megaport = new Megaport('https://api.megaport.com/v2/'); // with NEW version megaport.js
 //console.log(megaport.toString());
 
 megaport.auth({
-  //username: 'narusan@mac.com',
-  //password: 'megaport123',
+  //username: '',
+  //password: '',
 	token: 'c0edb666-0d14-436f-9c0d-8d4006d2773c'
 });
 
 megaport.ready(
   function(credentials) {
 
-    console.log('--- credentials---');
     console.log(credentials);
-    console.log('--- credentials---');
 
 		let loc = megaport.lists('locations').then(
       function(locations) {
-        //console.log(locations);
+        console.log(locations);
 				return locations;
       }
     );
@@ -25,89 +23,3 @@ megaport.ready(
 	}
 );
 
-const vm = new Vue({
-  el: '#location',
-  data: {
-		loc: void(0),
-  },
-  methods: {
-   	ll: function(){
-			console.log("ll called!");
-			megaport.lists('locations').then(
-				function(locations){
-					console.log(locations);
-					vm.loc = locations;
-				} 
-			);
-		},
-  }
-});
-
-
-/*
-
-		megaport.company(credentials.companyId).update({
-		  tradingName: 'AAAAAAAAAAAA',
-		}).then(
-		  function (response) {
-				console.log(response);
-			}
-		);
-
-
-		megaport.company(credentials.companyId).then(
-			function (profileObj) {
-				console.log(profileObj);
-			}
-		);
-
-
-    console.log('--- profile ---');
-		//megaport.profile(credentials.personId).then(
-		megaport.profile(credentials.personId).get().then(
-			function (profileObj) {
-				//output(profileObj);
-				console.log(profileObj);
-			}
-		);
-    console.log('--- profile ---');
-
-		megaport.lists('locations').then(
-			function(locations) {
-				console.log(locations);
-			}
-		);
-
-
-  }
-);
-
-//console.log(toString(conn));
-//console.log(util.inspect(conn.exports.credentials));
-//console.log(util.inspect(conn));
-
-megaport.company(conn.companyId).then(
-  function (profileObj) {
-    console.log(profileObj);
-  }
-);
-
-
-megaport.menuStats().then(
-  function (menustats) {
-    console.log(menustats);
-  }
-);
-
-megaport.failauth(function () {
-  console.log('Authentication Failed');
-});
-
-console.log('Menu stats');
-megaport.dashboard().then(
-  function (menustats) {
-    console.log(menustats);
-  }
-);
-
-*/
